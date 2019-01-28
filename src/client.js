c0d6500da8cc0489ae2d51ea633e96fdcdd3d3eb
ioedeveloper <daviddisu8@gmail.com>
2019-01-28 02:17:18 +0100

    Project Setup

---
+++ src/client.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Bootstrap from './vendor/bootstrap-native.min.js';
+import Layout from './pages/Layout'
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout />, app);
